a49de1f8db34a8707a78e094eee2c3609d1f31d8
Daniel Tonelli <d_tonelli@hotmail.com>
2022-05-08 01:51:34 -0300

    positivos

---
+++ estructuras-de-control/src/positivos.ts
@@ -0,0 +1,17 @@
+let cantTotal : number = 0;
+let cantPositivos : number = 0;
+let porcPositivos : number = 0;
+
+let num = Number(prompt("Ingrese un número"));
+while ( num != 0) {
+	if ( num > 0) {
+		cantPositivos++;
+	}
+	cantTotal++;
+	num = Number(prompt("Ingrese un número"));
+}
+if ( cantTotal > 0) {
+	porcPositivos=cantPositivos*100/cantTotal;
+	console.log(cantPositivos, "positivos ", porcPositivos, " % del total.");
+}
+	
