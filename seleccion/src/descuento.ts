26abcf876a4438096c570001181824e93e62e84b
Daniel Tonelli <d_tonelli@hotmail.com>
2022-05-16 14:19:34 -0300

    texto

--- seleccion/src/descuento.ts
+++ seleccion/src/descuento.ts
@@ -6,19 +6,19 @@ rotulo1.innerHTML = "Ingrese el monto: ";
 rotulo2.innerHTML = "Ingrese la cantidad: ";
 rotulo3.innerHTML = "Ingrese el mes: ";
 
-let dato1 = document.getElementById("dato1");
-let dato2 = document.getElementById("dato2");
-let dato3 = document.getElementById("dato3");
+let miDato1 = document.getElementById("dato1");
+let miDato2 = document.getElementById("dato2");
+let miDato3 = document.getElementById("dato3");
 
 let btnEnv = document.getElementById("btnEnviar");
 
 btnEnv.addEventListener("click", () => {
-  let monto: number = Number(dato1.value);
-  let cantidad: number = Number(dato2.value);
-  let mes: number = Number(dato3.value);
+  let monto: number = Number(miDato1.value);
+  let cantidad: number = Number(miDato2.value);
+  let mes: string = miDato3.value;
 
   let precioTotal: number = monto * cantidad;
-  if (mes === 10) {
+  if (mes == "Octubre") {
     let descuento: number = (precioTotal * 15) / 100;
     precioTotal -= descuento;
     console.log(
--- seleccion/src/par.ts
+++ seleccion/src/par.ts
@@ -1,21 +1,21 @@
-let rotulo = document.getElementById("rotulo");
+let miRotulo = document.getElementById("rotulo");
 /*
  Busca un Elemento HTML con el id que tiene el valor "rotulo" y
- asigna ese Elemento HTML a una variable cuyo nombre de variable elegí sea "rotulo"
+ asigna ese Elemento HTML a una variable cuyo nombre de variable elegí sea "miRotulo"
  (podría ser cualquier otro nombre de variable)
  El Elemento HTML en particular es un contenedor de párrafo: <p>
 */
-rotulo.innerHTML = "Ingrese el número: ";
+miRotulo.innerHTML = "Ingrese el número:";
 /*
  Carga el HTML "Ingrese el número: " dentro del contenedor
  referenciado con la variable de nombre "rotulo"
 */
-let dato = document.getElementById("dato");
+let miDato = document.getElementById("dato");
 
-let btnEnv = document.getElementById("btnEnviar");
+let miBoton = document.getElementById("btnEnviar");
 
-btnEnv.addEventListener("click", () => {
-  let numero: number = Number(dato.value);
+miBoton.addEventListener("click", () => {
+  let numero: number = Number(miDato.value);
   let resultado: string = "no es par ni impar.";
   if (numero > 0) {
     resultado = "es ";
